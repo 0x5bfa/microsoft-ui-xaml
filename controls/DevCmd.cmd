417013807173f4500838142953b5808b915fbcc4
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2026-05-31 11:14:59 +0900

    Move controls developer command under shell tools

--- controls/DevCmd.cmd
+++ controls/DevCmd.cmd
@@ -1,19 +1,2 @@
-@echo OFF
-
-pushd %~dp0
-
-set PATH=%PATH%;%~dp0\tools
-
-call %~dp0\tools\Shell\addaliases.cmd
-
-"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -Latest -requires Microsoft.Component.MSBuild -property InstallationPath > %TEMP%\vsinstalldir.txt
-
-set /p _VSINSTALLDIR15=<%TEMP%\vsinstalldir.txt
-
-call "%_VSINSTALLDIR15%\Common7\Tools\VsDevCmd.bat"
-
-pushd %~dp0
-
-if '%1%' neq '/PreserveContext' (
-    cmd /k
-)
+@call "%~dp0tools\Shell\DevCmd.cmd" %*
+@exit /b %ERRORLEVEL%
